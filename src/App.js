6e7ba30cdd60942278f7b716b1f4ba65f2065ca9
aldi <aldi@enablr.id>
2022-01-15 17:24:53 +0700

    reducer create data

--- src/App.js
+++ src/App.js
@@ -3,49 +3,57 @@ import Layout from "./component/layouts";
 import { List } from "./component/molecules";
 import { useSelector, useDispatch } from "react-redux";
 import { createData, initialValues } from "redux/reducer/reducer";
+import { useEffect } from "react";
+
+const dummy = [
+  {
+    id: 1,
+    title: "Make a meal",
+    description: "lorem ipsum",
+    status: 0,
+    createdAt: "2019-11-15 18:00",
+  },
+  {
+    id: 2,
+    title: "Dinner with family",
+    description: "lorem ipsum",
+    status: 0,
+    createdAt: "2019-11-16 18:00",
+  },
+  {
+    id: 3,
+    title: "Watch scary movie",
+    description: "lorem ipsum",
+    status: 0,
+    createdAt: "2019-11-15 13:00",
+  },
+  {
+    id: 4,
+    title: "Learn something new",
+    description: "lorem ipsum",
+    status: 1,
+    createdAt: "2019-11-15 08:00",
+  },
+  {
+    id: 5,
+    title: "Make a phone call to mom",
+    description: "lorem ipsum",
+    status: 1,
+    createdAt: "2019-11-15 04:00",
+  },
+];
 
 function App() {
-  const dummy = [
-    {
-      id: 1,
-      title: "Make a meal",
-      description: "lorem ipsum",
-      status: 0,
-      createdAt: "2019-11-15 18:00",
-    },
-    {
-      id: 2,
-      title: "Dinner with family",
-      description: "lorem ipsum",
-      status: 0,
-      createdAt: "2019-11-16 18:00",
-    },
-    {
-      id: 3,
-      title: "Watch scary movie",
-      description: "lorem ipsum",
-      status: 0,
-      createdAt: "2019-11-15 13:00",
-    },
-    {
-      id: 4,
-      title: "Learn something new",
-      description: "lorem ipsum",
-      status: 1,
-      createdAt: "2019-11-15 08:00",
-    },
-    {
-      id: 5,
-      title: "Make a phone call to mom",
-      description: "lorem ipsum",
-      status: 1,
-      createdAt: "2019-11-15 04:00",
-    },
-  ];
   const data = useSelector((state) => state.data.values);
   const isEmpty = useSelector((state) => state.data.empty);
 
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isEmpty) {
+      dispatch(initialValues(dummy));
+    }
+  }, [dispatch, isEmpty]);
 
   return (
     <Layout>
@@ -55,13 +63,27 @@ function App() {
             {!isEmpty && <List data={data} action="hover" />}
           </div>
         </Card>
-        <Button onClick={() => dispatch(initialValues(dummy))}>Create</Button>
+        <Button
+          onClick={() =>
+            dispatch(
+              createData({
+                id: 6,
+                title: "Make a phone call to mom1",
+                description: "lorem ipsum1",
+                status: 1,
+                createdAt: "2019-11-15 04:00",
+              })
+            )
+          }
+        >
+          Create
+        </Button>
       </div>
 
       <div className="w-full h-fit flex flex-col gap-4 items-center justify-center">
         <Card rounded shadow padding={false}>
           <div className="h-96 w-80 p-2 overflow-auto">
-            {!isEmpty && <List data={data} action="hover" />}
+            {!isEmpty && <List data={data} action="" />}
           </div>
         </Card>
       </div>
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -23,12 +23,8 @@ export const counterSlice = createSlice({
         empty: false,
       };
     },
-    createData: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
+    createData: (state, action) => {
+      return { ...state, values: [...state.values, { ...action.payload }] };
     },
     removeData: (state) => {
       state.value -= 1;
